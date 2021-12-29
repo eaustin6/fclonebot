0fc6b4f8d0ca5f472f4004f8387b3997d56d1e25
Eric Austin <86404384+eaustin6@users.noreply.github.com>
2021-12-29 11:16:05 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-npm install http-server -g
-http-server -p $PORT &
+
+chmod +x gclone
+echo "[General]
+path_to_gclone = ./gclone
+telegram_token = $telegram_token
+user_ids = $user_ids
+group_ids = $group_ids
+gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+python3 clever.py &
 python3 telegram_gcloner/telegram_gcloner.py
